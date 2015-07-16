02d0a62da94b038fad0546e743972423d0847903
Jérôme Velociter <jerome@velociter.fr>
2015-07-16 15:56:51 +0200

    Rebrand, remove some (unused) stuff

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,25 +1,10 @@
 package cool.naze
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.codahale.metrics.MetricRegistry.name
 import http.Server
 import http.Status
-import http.ok
 import http.response
-
+import org.slf4j.LoggerFactory
+import java.io.IOException
 
 object app {
 
@@ -27,14 +12,8 @@ object app {
 
     private val logger = LoggerFactory.getLogger(::main.javaClass);
 
-    private final val metrics = MetricRegistry();
-
     fun start(): Unit {
         Runtime.getRuntime().addShutdownHook(Thread("shutdown"))
-
-        val start = metrics.timer(name(javaClass<app>(), "server"))
-        val context = start.time()
-
         try {
             server.configure {
                 routes {
@@ -45,8 +24,7 @@ object app {
                 }
             }
             server.start()
-            val time = context.stop()
-            logger.info("Server started in {} ms", TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS))
+            logger.info("Server started")
             logger.info("CTRL^C to exit..")
             Thread.currentThread().join()
         } catch (e: IOException) {
